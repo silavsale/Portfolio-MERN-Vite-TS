39cbba238568eab9d09a6f3adeb692948aa234ec
MoonWalker <silavsale@gmail.com>
2023-02-04 22:11:19 -0500

    add: tailwind css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,24 @@
-import { useState } from "react";
-import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { NewNote } from "./Components/NewNote";
 
 function App() {
-  return <h1>Hi</h1>;
+  return (
+    <div className="container mx-auto my-4">
+      <Routes>
+        <Route
+          path="/"
+          element={<h1 className="text-3xl font-bold relative">Hello</h1>}
+        />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>}></Route>
+          <Route path="edit" element={<h1>Edit</h1>}></Route>
+        </Route>
+        <Route path="/new" element={<h1>New</h1>} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/Components/NewNote.tsx
@@ -0,0 +1,7 @@
+export function NewNote() {
+  return (
+    <div>
+      <h1 className="mb-4 bg-slate-400">New Note</h1>;
+    </div>
+  )
+}
